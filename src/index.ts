45dda879f2cbcfb8ab83dd5c67c2f31c5ee91361
Jhorman Tito <jhormanrus@gmail.com>
2023-07-13 22:01:03 +0000

    improve performance

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import Database from 'bun:sqlite'
 import { Router } from './router'
 
+const db = new Database('./src/database/himnario.db')
+
 const server = Bun.serve({
   fetch (request) {
     const router = new Router(request)
 
     router.get('/hymn', (params, response) => {
-      const db = new Database('./src/database/himnario.db')
       const query = db.query('SELECT id, number, title FROM hymn')
       return response.json(query.all())
     })
 
     router.get('/hymn/:id', (params, response) => {
       const hymnId = params.id
-      const db = new Database('./src/database/himnario.db')
       const hymn = db
         .query('SELECT id, number, title, mp3Url, mp3UrlInstr, mp3Filename FROM hymn WHERE id = ?1')
         .get(hymnId)
